0cf3a1a3fe35ea0eba7b33c6fd9ba703658872a2
Mikhail Nasyrov <streambot@gmail.com>
2018-08-27 10:07:33 +0700

    Updated dev dependencies. Replaced coffescript by javascript in unit tests.

---
+++ test/basic.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const should = require('should');
+const {exec} = require('child_process');
+
+describe('sanity check', () => {
+    it('should pass', () => {
+        assert(1 + 1 === 2);
+    });
+});
+
+describe('code-stats', () => {
+    describe('code-stats <paths>', () => {
+        it('should show line counts for each type', (done) => {
+            exec('./code-stats test/fixtures', (err, stdout) => {
+                should.not.exist(err);
+                const expected = `
+ Filetype | Line count
+-----------------------
+       js | 1000
+        c | 900
+       rb | 800
+       py | 700
+     java | 600
+~~~~~~~~~~~~~~~~~~~~~~~
+      All | 4000
+`;
+                stdout.should.equal(expected);
+                done();
+            });
+        });
+    });
+
+    describe('-x, --exclude <pattern>', () => {
+        it('should exclude files in addition to defaults', (done) => {
+            exec("./code-stats -x 'js|java' test/fixtures", (err, stdout) => {
+                should.not.exist(err);
+                const expected = `
+ Filetype | Line count
+-----------------------
+        c | 900
+       rb | 800
+       py | 700
+~~~~~~~~~~~~~~~~~~~~~~~
+      All | 2400
+`;
+                stdout.should.equal(expected);
+                done();
+            });
+        });
+    });
+});
